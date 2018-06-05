5e96fbc93fdde8c1432533e408c5fca55f592115
Edno Fedulo <edno.moraes@gmail.com>
2018-06-05 12:53:26 -0300

    initial commit

--- udacity-virtual-tourist/AppDelegate.swift
+++ udacity-virtual-tourist/AppDelegate.swift
@@ -13,10 +13,12 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    let dataController = DataController(modelName: "udacity_virtual_tourist")
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        var pin = Pin(context: dataController.viewContext)
+        
         return true
     }
 
